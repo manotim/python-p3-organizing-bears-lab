84d7ee266c47a48e030e75303f64c3bcc9152f64
manoti <manotimike@gmail.com>
2023-05-21 16:59:06 +0300

    done

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('nivalis', 7, 'M', 'Grey', 'Choleric', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('korshinskyi', 4, 'M', 'Black', 'Melancholic', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('pashia', 3, 'M', 'Brown', 'Phlegmatic', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('persica', 5, 'M', 'White', 'Sanguine', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('phaeocarpa', 5, 'F', 'Black-white', 'Sanguine', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('pyraster', 7, 'F', 'Gray', 'Choleric', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('pyraster', 7, 'F', 'Gray', 'Choleric', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 7, 'M', 'Green', 'Choleric', 1);
+
+
+
